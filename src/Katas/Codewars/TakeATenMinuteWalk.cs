74a4c378043ae3e03d5cc7226ba71c00ce5bec80
Graham King <graham.king@nice.com>
2024-02-05 14:33:29 +0000

    TakeATenMinuteWalk

---
+++ src/Katas/Codewars/TakeATenMinuteWalk.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Katas;
+
+/// <summary>
+/// https://www.codewars.com/kata/54da539698b8a2ad76000228/train/csharp
+/// </summary>
+public class TakeATenMinuteWalk
+{
+    private static Dictionary<string, int> latitudes = new() { { "e", 1 }, { "w", -1 } };
+
+    private static Dictionary<string, int> longitudes = new() { { "n", 1 }, { "s", -1 } };
+
+    public static bool IsValidWalk(string[] walk)
+    {
+        if (walk.Length != 10)
+        {
+            return false;
+        }
+
+        var latitude = 0;
+        var longitude = 0;
+        foreach (var direction in walk)
+        {
+            if (latitudes.TryGetValue(direction, out var latitudeMovementSize))
+            {
+                latitude += latitudeMovementSize;
+                continue;
+            }
+
+            if (longitudes.TryGetValue(direction, out var longitudeMovementSize))
+            {
+                longitude += longitudeMovementSize;
+                continue;
+            }
+        }
+
+        return latitude == 0 && longitude == 0;
+    }
+}
---
+++ test/Katas.Tests/Codewars/TakeATenMinuteWalkTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Katas;
+
+public class TakeATenMinuteWalkTests
+{
+    private static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData((object)new[] { "n", "s", "n", "s", "n", "s", "n", "s", "n", "s" }).Returns(true);
+            yield return new TestCaseData((object)new[] { "w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e" }).Returns(false);
+            yield return new TestCaseData((object)new[] { "w" }).Returns(false);
+            yield return new TestCaseData((object)new[] { "n", "n", "n", "s", "n", "s", "n", "s", "n", "s" }).Returns(false);
+        }
+    }
+
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public bool Test(string[] input) => TakeATenMinuteWalk.IsValidWalk(input);
+}
