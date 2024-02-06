e4c35882229fe11b232a554cb3f5c060ce178699
Graham King <graham.king@nice.com>
2024-02-06 14:08:47 +0000

    OnesAndZeros

---
+++ src/Katas/Codewars/OnesAndZeros.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Katas;
+
+/// <summary>
+/// https://www.codewars.com/kata/578553c3a1b8d5c40300037c/train/csharp
+/// </summary>
+public class OnesAndZeros
+{
+    public static int binaryArrayToNumber(int[] BinaryArray)
+    {
+        return Convert.ToInt32(string.Join(string.Empty, BinaryArray), 2);
+    }
+}
---
+++ test/Katas.Tests/Codewars/OnesAndZerosTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Katas;
+
+public class OnesAndZerosTests
+{
+    private static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData((object?)new int[]{0,0,0,0}).Returns(0);
+            yield return new TestCaseData((object?)new int[]{1,1,1,1}).Returns(15);
+            yield return new TestCaseData((object?)new int[]{0,1,1,0}).Returns(6);
+            yield return new TestCaseData((object?)new int[]{0,1,0,1}).Returns(5);
+        }
+    }
+
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int Test(int[] binaryArray) => OnesAndZeros.binaryArrayToNumber(binaryArray);
+}
