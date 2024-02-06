9b6e9dea90db5a27ad184c321ba60bf301d84bc9
Graham King <graham.king@nice.com>
2024-02-06 14:15:09 +0000

    FindTheOddInt

---
+++ src/Katas/Codewars/FindTheOddInt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Katas;
+
+/// <summary>
+/// https://www.codewars.com/kata/54da5a58ea159efa38000836
+/// </summary>
+public class FindTheOddInt
+{
+    public static int find_it(int[] seq)
+    {
+        return seq
+            .GroupBy(value => value)
+            .Single(group => group.Count() % 2 == 1)
+            .Key;
+    }
+
+}
---
+++ test/Katas.Tests/Codewars/FindTheOddIntTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Katas;
+
+public class FindTheOddIntTests
+{
+    private static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData((object?)new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }).Returns(5);
+        }
+    }
+
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int Test(int[] sequence) => FindTheOddInt.find_it(sequence);
+}
