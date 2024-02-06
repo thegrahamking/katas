12dbc03726ca8434c2b42d88405a77b6afb0eb27
Graham King <graham.king@nice.com>
2024-02-06 13:43:22 +0000

    GrowthOfAPopulation

---
+++ src/Katas/Codewars/GrowthOfAPopulation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Katas;
+
+/// <summary>
+/// https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/csharp
+/// </summary>
+public class GrowthOfAPopulation
+{
+    public static int NbYear(int p0, double percent, int aug, int p)
+    {
+        int currentPopulation = p0;
+        var migrantsInPerYear = aug;
+        var percentageIncreasePerYear = percent / 100;
+
+        var expectedPopulation = p;
+
+        var years = 0;
+
+        while (currentPopulation < expectedPopulation)
+        {
+            var populationGrowth = (int)(currentPopulation * percentageIncreasePerYear);
+            currentPopulation = currentPopulation + populationGrowth + migrantsInPerYear;
+            years++;
+        }
+
+        return years;
+    }
+}
---
+++ test/Katas.Tests/Codewars/GrowthOfAPopulationTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Katas;
+
+public class GrowthOfAPopulationTests
+{
+    private static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData(1500, 5, 100, 5000).Returns(15);
+            yield return new TestCaseData(1500000, 2.5, 10000, 2000000).Returns(10);
+            yield return new TestCaseData(1500000, 0.25, 1000, 2000000).Returns(94);
+        }
+    }
+
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int Test(int p0, double percent, int aug, int p) => GrowthOfAPopulation.NbYear(p0, percent, aug, p);
+}
